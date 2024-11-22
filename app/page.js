2e646dc5413f7eeaf1bf564c1479cdfa6997252a
shafeeq <shafeeqmenoth1@gmail.com>
2024-11-22 12:36:07 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
+import { PiInstagramLogoFill } from "react-icons/pi";
+import { RiYoutubeFill } from "react-icons/ri";
+import { FaFacebook } from "react-icons/fa";
 
 export default function Home() {
   return (
-  
+    <div className=" wrapper">
+      <img src="/logo.png"/>
+    <h1>coming soon<span className="dot">.</span></h1>
+    <p>We are under construction.</p>
+    <div className="icons flex gap-1 justify-center ">
+      <Link className="h-12 w-12 bg-white rounded-full flex justify-center items-center"  href=""><PiInstagramLogoFill className="text-black text-2xl" /></Link>
+      <Link className="h-12 w-12 bg-white rounded-full flex justify-center items-center" href=""><RiYoutubeFill className="text-black text-2xl" /></Link>
+      <Link className="h-12 w-12 bg-white rounded-full flex justify-center items-center" href=""><FaFacebook className="text-black text-2xl" /></Link>
+ 
+    </div>
+   </div>
   );
 }
